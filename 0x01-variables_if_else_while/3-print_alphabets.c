b12e644940f7b94603c6fd986d985b8508608601
omotosho5038 <adebayoayomide5038@gmail.com>
2022-07-13 00:21:57 -0700

    all tasks commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase, and then in uppercase,
+ * folloewd by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+
+}
