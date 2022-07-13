8fdb73bb1b194171cada2f34caed8c94e94a1bd1
omotosho5038 <adebayoayomide5038@gmail.com>
2022-07-13 00:48:21 -0700

    all tasks

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter = 'a';
+
+	while (count < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+
+		count++;
+	}
+}
